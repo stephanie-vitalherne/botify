50abee1d7097c3b4ecb09b69223f5f2b4b2b6756
Stephanie Vital-Herne <vitalherne@gmail.com>
2021-12-27 18:36:01 -0500

    using recoil to change the state of color accent in the header and change the playlist that is in view for show

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -9,11 +9,14 @@ import {
 import { signOut, useSession } from "next-auth/react";
 import { useState, useEffect } from "react";
 import useSpotify from "../hooks/useSpotify";
+import { useRecoilState } from "recoil";
+import { playlistIdState } from "../atoms/playlistAtom";
 
 function Sidebar() {
   const spotifyApi = useSpotify();
   const { data: session, status } = useSession();
   const [playlists, setPlaylists] = useState([]);
+  const [playlistId, setPlaylistId] = useRecoilState(playlistIdState);
 
   useEffect(() => {
     if (spotifyApi.getAccessToken()) {
@@ -65,7 +68,11 @@ function Sidebar() {
 
         {/* Playlists rendering */}
         {playlists.map((playlist) => (
-          <p key={playlist.id} className="cursor-pointer hover:text-white">
+          <p
+            key={playlist.id}
+            onClick={() => setPlaylistId(playlist.id)}
+            className="cursor-pointer hover:text-white"
+          >
             {playlist.name}
           </p>
         ))}
