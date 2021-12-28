ed69b1018d5fb7933cc23fe4d85c748337723f88
Stephanie Vital-Herne <vitalherne@gmail.com>
2021-12-28 00:25:37 -0500

    clone can play once its linked to a device (the regular app)

--- components/Song.js
+++ components/Song.js
@@ -1,11 +1,27 @@
 import useSpotify from "../hooks/useSpotify";
 import { millisToMinutesAndSeconds } from "../lib/time";
+import { currentTrackIdState, isPlayingState } from "../atoms/songAtom";
+import { useRecoilState } from "recoil";
 
 function Song({ order, track }) {
   const spotifyApi = useSpotify();
+  const [currentTrackId, setCurrentTrackId] =
+    useRecoilState(currentTrackIdState);
+  const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
+
+  const playSong = () => {
+    setCurrentTrackId(track.track.id);
+    setIsPlaying(true);
+    spotifyApi.play({
+      uris: [track.track.uri],
+    });
+  };
 
   return (
-    <div className="grid grid-cols-2 text-gray-500 py-4 px-5 hover:bg-gray-900 rounded-lg cursor-pointer">
+    <div
+      onClick={playSong}
+      className="grid grid-cols-2 text-gray-500 py-4 px-5 hover:bg-gray-900 rounded-lg cursor-pointer"
+    >
       <div className="flex items-center space-x-4">
         <p>{order + 1}</p>
         <img
